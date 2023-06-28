bb3bd70321f99366bf5937ae9a7b1065f48eb812
Nick Parker <nick@nickbp.com>
2023-06-28 19:34:13 +1200

    Listen for added/removed devices, pairing them with dynamic event handlers

---
+++ src/devicewatch.rs
@@ -0,0 +1,160 @@
+use anyhow::{bail, Context, Result};
+use async_std::task;
+use evdev::{Device, EventStream, EventType};
+use futures::StreamExt;
+use notify::Watcher;
+use tracing::{debug, info, trace, warn};
+
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+enum DeviceEventKind {
+    Created,
+    Deleted,
+}
+
+#[derive(Debug)]
+struct DeviceEvent {
+    pub kind: DeviceEventKind,
+    pub path: PathBuf,
+}
+
+pub trait DeviceHandler: Send + 'static {
+    fn handle_device_stream(&mut self, stream: EventStream) -> Result<task::JoinHandle<()>>;
+}
+
+pub async fn watch_loop<F: DeviceHandler>(mut handler: F) -> Result<()> {
+    // Start watch for new and removed devices BEFORE scanning current devices.
+    let (device_event_tx, mut device_event_rx): (
+        async_channel::Sender<DeviceEvent>,
+        async_channel::Receiver<DeviceEvent>,
+    ) = async_channel::bounded(32);
+    let mut watcher = notify::RecommendedWatcher::new(
+        move |res: Result<notify::Event, notify::Error>| {
+            match res {
+                Ok(event) => send_device_events(event, &device_event_tx),
+                Err(e) => warn!("filesystem watch error: {:?}", e),
+            }
+        },
+        notify::Config::default(),
+    ).context("failed to init watcher")?;
+    watcher.watch(std::path::Path::new("/dev/input"), notify::RecursiveMode::NonRecursive)?;
+
+    // Scan current devices
+    let mut devices = HashMap::new();
+    for (path, device) in evdev::enumerate() {
+        // enumerate already filters for 'event*' filenames
+        if !compatible_device_events(&device) {
+            trace!("Ignoring incompatible device: {} @ {}", device.name().unwrap_or("(Unnamed device)"), path.to_string_lossy());
+        }
+        let stream = start_device_stream(device, &path)?;
+        devices.insert(path, handler.handle_device_stream(stream)?);
+    }
+    info!("Handling {} initial devices", devices.len());
+    if devices.len() <= 0 {
+        bail!("Didn't find any compatible devices, are you root?");
+    }
+
+    // Start handler to consume new/removed device events
+    while let Some(event) = device_event_rx.next().await {
+        info!("device file event: {:?}", event);
+        match event.kind {
+            DeviceEventKind::Created => {
+                if !compatible_device_path(&event.path) {
+                    continue;
+                }
+                match Device::open(&event.path) {
+                    Ok(device) => {
+                        if !compatible_device_events(&device) {
+                            trace!("Ignoring incompatible device: {} @ {}", device.name().unwrap_or("(Unnamed device)"), event.path.to_string_lossy());
+                            continue;
+                        }
+                        match start_device_stream(device, &event.path) {
+                            Ok(stream) => {
+                                match handler.handle_device_stream(stream) {
+                                    Ok(join_handle) => {
+                                        devices.insert(event.path, join_handle);
+                                        info!("Handling {} devices following addition", devices.len());
+                                    },
+                                    Err(e) => {
+                                        warn!("Failed to start event handler for device {}: {}", event.path.to_string_lossy(), e);
+                                    },
+                                }
+                            },
+                            Err(e) => {
+                                // Avoid exiting loop and aborting program if a new device fails
+                                warn!("Failed to read device {}: {}", event.path.to_string_lossy(), e);
+                            }
+                        }
+                    },
+                    Err(e) => {
+                        // Avoid exiting loop and aborting program if a new device fails
+                        warn!("Failed to init device {}: {}", event.path.to_string_lossy(), e);
+                    }
+                };
+            },
+            DeviceEventKind::Deleted => {
+                if let Some(join_handle) = devices.remove(&event.path) {
+                    info!("Removing device: {}", event.path.to_string_lossy());
+                    join_handle.cancel().await;
+                }
+                info!("Handling {} devices following removal", devices.len());
+            },
+        }
+    }
+    Ok(())
+}
+
+fn compatible_device_path(path: &PathBuf) -> bool {
+    // Filename should be 'event<N>', like 'event3' or 'event14'
+    path.file_name()
+        .filter(|f| f.to_string_lossy().starts_with("event"))
+        .is_some()
+}
+
+fn compatible_device_events(d: &Device) -> bool {
+    // We care about these kinds of devices: keyboard, touchpad, and mouse, respectively
+    let evts = d.supported_events();
+    evts.contains(EventType::KEY) || evts.contains(EventType::ABSOLUTE) || evts.contains(EventType::RELATIVE)
+}
+
+fn start_device_stream(device: Device, path: &PathBuf) -> Result<EventStream> {
+    let device_name = device.name().unwrap_or("(Unnamed device)").to_string();
+    info!(
+        "Input device: {} @ {}, {:?}, {:?}, {:?}",
+        device_name,
+        path.to_string_lossy(),
+        device.properties(),
+        device.supported_events(),
+        device.supported_keys()
+    );
+    device.into_event_stream()
+        .with_context(|| format!("Failed to initialize async fd for device: {} @ {}", device_name, path.to_string_lossy()))
+}
+
+fn send_device_events(event: notify::Event, device_event_tx: &async_channel::Sender<DeviceEvent>) {
+    match event.kind {
+        notify::EventKind::Create(notify::event::CreateKind::File) => {
+            debug!("file created: {:?}", event);
+            task::block_on(async {
+                for path in event.paths {
+                    if let Err(e) = device_event_tx.send(DeviceEvent{kind: DeviceEventKind::Created, path}).await {
+                        warn!("Failed to queue device create event: {}", e);
+                    }
+                }
+            });
+        },
+        notify::EventKind::Remove(notify::event::RemoveKind::File) => {
+            debug!("file deleted: {:?}", event);
+            task::block_on(async {
+                for path in event.paths {
+                    if let Err(e) = device_event_tx.send(DeviceEvent{kind: DeviceEventKind::Deleted, path}).await {
+                        warn!("Failed to queue device delete event: {}", e);
+                    }
+                }
+            })
+        },
+        _ => trace!("other filesystem event: {:?}", event),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,14 @@
+mod devicewatch;
 mod logging;
 
 use anyhow::{anyhow, bail, Result};
 use async_std::task;
-use evdev::{Device, EventType, InputEvent, Key};
+use devicewatch::DeviceHandler;
+use evdev::{EventStream, EventType, InputEvent, Key};
 use futures::StreamExt;
-use futures_lite::future::FutureExt;
 use tracing::{error, info, warn};
 
 use std::str::FromStr;
-
-fn compatible_device(d: &Device) -> bool {
-    let evts = d.supported_events();
-    evts.contains(EventType::KEY) || evts.contains(EventType::ABSOLUTE) || evts.contains(EventType::RELATIVE)
-}
-
-fn pick_devices() -> Result<Vec<Device>> {
-    let devices = evdev::enumerate()
-        .map(|t| t.1)
-        .filter(|d| compatible_device(d))
-        .collect::<Vec<_>>();
-    if devices.len() <= 0 {
-        bail!("Didn't find any compatible devices");
-    }
-    for d in &devices {
-        info!("- {}, {:?}, {:?}, {:?}", d.name().unwrap_or("(Unnamed device)"), d.properties(), d.supported_events(), d.supported_keys());
-    }
-    Ok(devices)
-}
 
 fn check_combo(event: &InputEvent, combo_keys: &Vec<Key>, keys_on: &mut bit_vec::BitVec) -> bool {
     if event.event_type() == EventType::KEY {
@@ -51,80 +33,59 @@ fn check_combo(event: &InputEvent, combo_keys: &Vec<Key>, keys_on: &mut bit_vec:
     return false;
 }
 
+async fn read_device_events(mut stream: EventStream, combo_keys: &Vec<Key>) {
+    let mut keys_on = bit_vec::BitVec::from_elem(combo_keys.len(), false);
+    while let Some(event) = stream.next().await {
+        match event {
+            Ok(event) => {
+                if check_combo(&event, &combo_keys, &mut keys_on) {
+                    // Combo has been reached on this device
+                    // TODO emit signal to switch output target, which will then signal this device task to grab or ungrab the device
+                    info!("COMBO!!!!!");
+                } else {
+                    info!("event for {:?}: {:?}", stream.device().name(), event);
+                }
+            },
+            Err(e) => {
+                // Common when the device has been unplugged.
+                // We'll frequently get this error just as inotify is telling us the file is deleted.
+                // Exit to avoid an infinite loop on trying to read the missing file.
+                warn!("Error event for {:?}, removing device: {:?}", stream.device().name(), e);
+                return;
+            },
+        }
+    }
+}
+
+struct ComboHandler {
+    combo_keys: Vec<Key>,
+}
+
+impl DeviceHandler for ComboHandler {
+    fn handle_device_stream(&mut self, stream: EventStream) -> Result<task::JoinHandle<()>> {
+        let combo_keys = self.combo_keys.clone();
+        task::Builder::new().name(format!("device: {:?}", stream.device().name())).spawn(async move {
+            read_device_events(stream, &combo_keys).await
+        }).map_err(|e| anyhow!(e))
+    }
+}
+
 fn main() -> Result<()> {
     logging::init_logging();
 
     // TODO args for user-specified key combos. require at least one key
     let combo_keys = vec![
-        Key::from_str("KEY_LEFTCTRL").map_err(|e| anyhow!("Unsupported key: {:?}", e))?,
+        Key::from_str("KEY_RIGHTCTRL").map_err(|e| anyhow!("Unsupported key: {:?}", e))?,
         Key::from_str("KEY_LEFTSHIFT").map_err(|e| anyhow!("Unsupported key: {:?}", e))?,
         Key::from_str("KEY_F").map_err(|e| anyhow!("Unsupported key: {:?}", e))?,
     ];
+    let device_handler = ComboHandler{combo_keys};
 
-    // TODO update device tasks as devices are added or removed
-    let devices = pick_devices()?;
-    let devices = task::block_on(async move {
-        devices
-            .into_iter()
-            .filter_map(|d| {
-                match d.into_event_stream() {
-                    Ok(s) => {
-                        Some(s)
-                    },
-                    Err(e) => {
-                        // Skip this device? Something wrong with it, or race where it was just removed?
-                        warn!("Failed to initialize async fd for device: {}", e);
-                        None
-                    }
-                }
-            })
-            .collect::<Vec<_>>()
-    });
-    let mut tasks: Vec<async_std::task::JoinHandle<()>> = Vec::new();
-    for mut d in devices {
-        let combo_keys = combo_keys.clone();
-        tasks.push(task::Builder::new().name(format!("device: {:?}", d.device().name())).spawn(async move {
-            // TODO when told by an upstream signal, grab or ungrab the d.device().
-            // this should be controlled via the system switcher shortcut
-
-            let mut keys_on = bit_vec::BitVec::from_elem(combo_keys.len(), false);
-            while let Some(event) = d.next().await {
-                match event {
-                    Ok(event) => {
-                        if check_combo(&event, &combo_keys, &mut keys_on) {
-                            // Combo has been reached on this device
-                            // TODO emit signal to switch output target (and grab or ungrab local devices)
-                            info!("COMBO!!!!!");
-                        } else {
-                            info!("got event for {:?}: {:?}", d.device().name(), event);
-                        }
-                    },
-                    Err(e) => warn!("Got error event for {:?}: {:?}", d.device().name(), e),
-                }
-            }
-        })?);
-    }
-
-    let poller = TaskPoller { tasks };
     task::block_on(async move {
-        error!("{}", poller.await);
-    });
-    bail!("a task exited, bailing")
-}
-
-struct TaskPoller {
-    tasks: Vec<task::JoinHandle<()>>,
-}
-
-impl std::future::Future for TaskPoller {
-    type Output = String;
-    fn poll(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
-        for task in &mut self.tasks {
-            if let std::task::Poll::Ready(_) = task.poll(cx) {
-                let msg = format!("task {:?} has exited, shutting down process", task.task().name());
-                return std::task::Poll::Ready(msg);
-            }
+        if let Err(e) = devicewatch::watch_loop(device_handler).await {
+            error!("input device watch failed: {}", e);
         }
-        std::task::Poll::Pending
-    }
+    });
+
+    bail!("device watch loop exited, bailing")
 }
